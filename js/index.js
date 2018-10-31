d6185b3c5323eddbc17ef259397567a4067ca8ef
Pallav <pallav@email.com>
2018-11-01 00:38:41 +0530

    food section done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+    $("#siteLogo").on("click", function(){
+        $(window).location.reload();
+    });
+
     $("#clothesDonateButton").on("click", function(){
         $(".LandingPage").hide("slow");
         $("#clothesDonate").show("slow");
     });
+
+    $("#foodDonateButton").on("click", function(){
+        $(".LandingPage").hide("slow");
+        $("#foodDonate").show("slow");
+    });
+
 
 });
